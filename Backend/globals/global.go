d2bdadd89f2c3edf56b3d2419a969d58c3986975
nearlynithin <nearlynithin@gmail.com>
2025-07-18 20:59:45 +0530

    feat: User and Email object

--- Backend/globals/global.go
+++ Backend/globals/global.go
@@ -1,8 +1,24 @@
 package globals
 
-import(
+import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+type User struct {
+	Github_id int64 `json:"id"`
+	Username string `json:"login"`
+	Github_url string `json:"html_url"`
+	Avatar_url string `json:"avatar_url"`
+	SessionToken string
+	Email string
+}
+
+type Email struct {
+	Email string `json:"email"`
+	Primary bool `json:"primary"`
+	Verified bool `json:"verified"`
+	Visibility string `json:"visibility"`
+}
 
 var Sessions = map[string]string{}
 
