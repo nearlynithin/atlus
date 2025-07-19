2eec8d073ba9c6dad9fa546784ab0a27d6b52e0d
nearlynithin <nearlynithin@gmail.com>
2025-07-19 13:10:33 +0530

    fix: checks before adding user, helpers for fetching user and updating session token, check for session expiry before fetching

--- Backend/handlers/connectDB.go
+++ Backend/handlers/connectDB.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -23,18 +25,32 @@ func InitDB() {
 
 	log.Println("Connected via pooler to:", version)
 	globals.DB = pool
+
+	forkPtr := flag.Bool("dev", false ,"DEV MODE : to truncate all db tables, on startup")
+	flag.Parse()
+	if *forkPtr {
+		globals.DB.Exec(ctx, "TRUNCATE table users, sessions;")
+		fmt.Println("truncated tables users and sessions!")
+	}
 }
 
-func addUser(ctx context.Context, user globals.User) {
+func addUser(ctx context.Context, user globals.User) (error) {
 	var inputID int
 	err := globals.DB.QueryRow(ctx,
 		`INSERT INTO users (github_id, username, github_url, avatar, email)
 		 VALUES ($1, $2, $3, $4, $5)
+		 ON CONFLICT (github_id) DO UPDATE SET
+			username = EXCLUDED.username,
+  			github_url = EXCLUDED.github_url,
+  			avatar = EXCLUDED.avatar,
+  			email = EXCLUDED.email
 		 RETURNING input_id`,
 		user.Github_id, user.Username, user.Github_url, user.Avatar_url, user.Email,
 	).Scan(&inputID)
+
 	if err != nil {
-		log.Fatal("Error inserting the user:", err)
+		fmt.Printf("Error adding user: %s", err)
+		return err
 	}
 
 	_, err = globals.DB.Exec(ctx,
@@ -43,8 +59,10 @@ func addUser(ctx context.Context, user globals.User) {
 		user.SessionToken, user.Github_id, inputID,
 	)
 	if err != nil {
-		log.Fatal("Error inserting session:", err)
+		fmt.Printf("Error adding session: %s", err)
+		return err
 	}
+	return nil
 }
 
 func fetchUser(ctx context.Context, sessionID string) (string, error) {
@@ -53,11 +71,43 @@ func fetchUser(ctx context.Context, sessionID string) (string, error) {
 	err := globals.DB.QueryRow(ctx,
 		`SELECT username FROM users
 		JOIN sessions s on s.github_id = users.github_id
-		WHERE s.session_id = $1`, sessionID).Scan(&username)
+		WHERE s.session_id = $1 AND s.expires_at > NOW()`, sessionID).Scan(&username)
 
 	if err != nil {
 		return "", err
 	}
 
 	return username, nil
+}
+
+func fetchUserByGithubID(ctx context.Context, githubID int64) (globals.User, error) {
+	var user globals.User
+
+	err := globals.DB.QueryRow(ctx, `
+		SELECT github_id, username, github_url, avatar, email  FROM users where github_id = $1
+	`, githubID).Scan(&user.Github_id, &user.Username, &user.Github_url,&user.Avatar_url, &user.Email )
+	if err != nil {
+		return user, err
+	}
+
+	// here, user does not have a session Token
+	return user, nil
+}
+
+func updateSessionToken(ctx context.Context, githubID int64, newSessionID string) (error) {
+
+	_, err := globals.DB.Exec(ctx, `
+		INSERT INTO sessions (github_id, session_id, expires_at)
+		VALUES ($1, $2, NOW() + INTERVAL '30 day')
+		ON CONFLICT (github_id)
+		DO UPDATE SET 
+		session_id = EXCLUDED.session_id
+		expires_at = EXCLUDED.expires_at
+	`, githubID, newSessionID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
