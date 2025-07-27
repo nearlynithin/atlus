90722b3a476a4c7d7bcfc92b51b72baf5a5deb41
nearlynithin <nearlynithin@gmail.com>
2025-07-27 10:31:45 +0530

    feat: new handler for leaderboard content

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/sceptix-club/atlus/Backend/handlers"
 )
 
-
 func main() {
 
 	err := godotenv.Load()
@@ -22,7 +21,6 @@ func main() {
 	globals.Hostname = os.Getenv("HOSTNAME")
 	globals.Port = os.Getenv("PORT")
 
-
 	handlers.InitDB()
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
@@ -30,17 +28,16 @@ func main() {
 	conf := handlers.InitOAuthConfig()
 	lf := handlers.LoginFlow{Conf: conf}
 
-	
-	mux.HandleFunc("/",  handlers.RootHandler(tpl))
+	mux.HandleFunc("/", handlers.RootHandler(tpl))
 	mux.HandleFunc("/login/", lf.GithubLoginHandler)
 	mux.HandleFunc("/logout/", handlers.GithubLogoutHandler)
 	mux.HandleFunc("/github/callback/", lf.GithubCallbackHandler)
-	mux.HandleFunc("/puzzles/{slug}",handlers.LevelHandler(tpl))
+	mux.HandleFunc("/puzzles/{slug}", handlers.LevelHandler(tpl))
 	mux.HandleFunc("/inputs/{slug}", handlers.InputHandler)
 	mux.HandleFunc("/submitAnswer/{slug}", handlers.SubmitAnswerHandler)
 	mux.HandleFunc("/leaderboard/", handlers.LeaderboardHandler(tpl))
+	mux.HandleFunc("/leaderboard/live", handlers.LeaderboardLiveHandler(tpl))
 
-
-	fmt.Printf("Listening on %s%s ...\n",globals.Hostname,globals.Port)
+	fmt.Printf("Listening on %s%s ...\n", globals.Hostname, globals.Port)
 	log.Panic(http.ListenAndServe(":"+globals.Port, mux))
 }
